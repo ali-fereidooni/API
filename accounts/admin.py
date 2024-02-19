11b1441ef8b45f6698b460cd58b8db2377cbfff9
alifereidooni <ali.fereidooni.a@gmail.com>
2024-02-19 10:45:17 -0800

    admin pannel and fixxing bugs

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,30 @@
 from django.contrib import admin
 from .models import User
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .forms import UserChangeForm, UserCreationForm
 
 
 class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('email', 'phone', 'is_admin')
+    list_filter = ('is_admin',)
+    readonly_fields = ('last_login',)
+
+    fieldsets = (
+        ('Main', {'fields': ('email', 'phone',
+         'first_name', 'last_name', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_admin', 'is_superuser',
+         'last_login', 'groups')}),
+
+    )
+
+    add_fieldsets = (
+        (None, {'fields': ('phone', 'email',
+         'first_name', 'last_name', 'password1', 'password2')}),
+    )
+
     list_display = (
         "phone", "first_name",
         "last_name", "is_staff",
@@ -22,6 +43,13 @@ class UserAdmin(BaseUserAdmin):
         "-pk",
     )
     list_per_page = 25
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        is_superuser = request.user.is_superuser
+        if not is_superuser:
+            form.base_fields['is_superuser'].disabled = True
+        return form
 
 
 admin.site.register(User, UserAdmin)
--- accounts/urls.py
+++ accounts/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 app_name = 'accounts'
-urlpatterns = {
+urlpatterns = [
     path('', views.UserList.as_view(), name='user-list'),
     path("users/<int:pk>/", views.UserDetailUpdateDeleteView.as_view(),
          name="users-detail"),
     path('register/', views.UserRegisterView.as_view(), name='register'),
-}
+]
